097c8515ccaad5a1d2906af8f45723cdf91f5e5c
Ken <kennethlieu@gmail.com>
2024-02-07 16:03:16 -0700

    Base menu, unit placer, unit template

---
+++ tower_def/unit.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var atk_speed: float
+var atk: int
+var range: float
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
