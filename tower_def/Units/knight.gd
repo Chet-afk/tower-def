cf96b455e0259511905799fc5bf9603a039ed83f
Ken <kennethlieu@gmail.com>
2024-03-02 18:03:06 -0700

    renamed some units. changed attacks to polygon bodies for odd shapes

--- tower_def/Units/knight.gd
+++ tower_def/Units/knight.gd
@@ -6,7 +6,8 @@ func _ready():
 func create_attack():
 	attack = attack_scene.instantiate()
 	attack.set_atk(self.get_atk())
-	attack.set_position(enemy_to_target)
+	attack.set_position(self.get_global_position())
+	attack.look_at(enemy_to_target)
 	attack.set_target(enemy_to_target)
 	# attach the bullets to the viewport 
 	# otherwise will follow this tower node if moved
