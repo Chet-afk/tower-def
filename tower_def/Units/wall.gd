724a2af01e86b38a51a8376c184dc5ffc4107892
Ken <kennethlieu@gmail.com>
2024-03-06 20:47:45 -0700

    HP bar for wall

--- tower_def/Units/wall.gd
+++ tower_def/Units/wall.gd
@@ -1,13 +1,21 @@
 extends StaticBody2D
 
-var hp: int
-
+var cur_hp: int
+var max_hp: int
 
 func _init():
-	hp = 1000
+	cur_hp = 1000
+	max_hp = 1000
+
+func _ready():
+	$HPBar.set_max(max_hp)
+	$HPBar.set_value(max_hp)
+	$HPBar/HPNum.set_text(str(max_hp) + " / " + str(max_hp))
 
 func take_damage(dmg: int):
-	hp -= dmg
-	if hp <= 0:
+	cur_hp -= dmg
+	$HPBar.value -= dmg
+	$HPBar/HPNum.set_text(str(cur_hp) + " / " + str(max_hp))
+	if cur_hp <= 0:
 		# Pause the game
 		get_tree().paused = true
